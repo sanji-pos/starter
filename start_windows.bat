f7e11ea30fb7335734f03a64a68fc9ba59566019
josedrojasa <josedrojasa@hotmail.com>
2024-10-05 18:22:42 -0500

    Add container

--- start_windows.bat
+++ start_windows.bat
@@ -1,22 +1,44 @@
 @echo off
-for /f "tokens=2 delims=:" %%A in ('ipconfig ^| findstr "Dirección IPv4"') do (
+setlocal
+
+:: Start Docker Desktop
+start "" "C:\Program Files\Docker\Docker\Docker Desktop.exe"
+
+:: Wait for Docker to start (adjust the timeout as needed)
+timeout 10
+
+:: Check and create volumes/pg directory if it doesn't exist
+if not exist "volumes\pg" (
+    mkdir "volumes"
+    mkdir "volumes\pg"
+)
+
+:: Get the IPv4 Address
+for /f "tokens=2 delims=:" %%A in ('ipconfig ^| findstr /i "IPv4"') do (
     set ip=%%A
 )
 
+:: Trim leading spaces from IP address
+set ip=%ip:~1%
+
+:: Pull the latest changes from the git repository
 git pull
 
-echo Running in %ip:~1%
+echo Running in %ip%
 
+:: Stop and remove the existing Docker container
 docker stop local-sanji
 timeout 5
-
 docker rm local-sanji
 timeout 5
 
+:: Prune unused images
 docker image prune -a -f --filter "until=730h"
 
+:: Pull the latest Docker image
 docker pull sanjidev/gateway:latest
 
-docker run -u nextjs --platform linux/amd64 -e HOST_PRIVATE_IP=%ip:~1% -p 3000:3000 -v C:\Sanji:/var/lib/postgresql/data -w /var/lib/postgresql/data --name local-sanji --rm sanjidev/gateway:latest
+:: Run the Docker container
+docker run -u nextjs --platform linux/amd64 -e HOST_PRIVATE_IP=%ip% -p 3000:3000 -v "%cd%\volumes\pg:/var/lib/postgresql/data" -w /var/lib/postgresql/data --name local-sanji --rm sanjidev/gateway:latest
 
-pause
+endlocal
